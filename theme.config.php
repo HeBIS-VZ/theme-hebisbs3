6df9e648eb4700885fb467610d637925473659f5
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2016-04-13 16:20:39 +0200

    hebis theme

---
+++ theme.config.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    "extends" => "bootstrap3",
+    "css" => [
+        'compiled.css',
+        'custom.css',
+        'icons.css'
+    ],
+    'js' => [
+        'vendor/bootstrap.min.js'
+    ],
+    /*
+    "less" => [
+        "active" => true,
+        "compiled.less"
+    ],
+    */
+];
